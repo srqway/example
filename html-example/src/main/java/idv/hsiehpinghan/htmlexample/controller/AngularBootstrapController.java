8b2118ad2121e0e3e4d3c7523ac3eb75d3168b79
thank <thank@localhost>
2015-09-09 17:32:41 +0800

    backup.

---
+++ html-example/src/main/java/idv/hsiehpinghan/htmlexample/controller/AngularBootstrapController.java
@@ -0,0 +1,16 @@
+package idv.hsiehpinghan.htmlexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/angularBootstrap")
+public class AngularBootstrapController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "angularBootstrap/index";
+	}
+
+}
