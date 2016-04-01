d56465db0039a45b5bb2eca27480dc2faf96cdf6
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-04-01 08:48:14 +0800

    backup.

--- python-example/class/basic.py
+++ python-example/class/basic.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from __future__ import division
 
 print("<<basic function>>")
-class class_0:
+class Class_0:
     """
     class document
     """
@@ -13,28 +13,52 @@ class class_0:
         self.x = x
     def function_0(self):
         return self.x;
-print(class_0(3).function_0())
+print(Class_0(3).function_0())
 
 print("<<class variable & instance variable>>")
-class class_1:
+class Class_1:
     class_variable = [];
     def __init__(self, x):
         self.instance_variable = x;
-class_1_0 = class_1(0)
-class_1_1 = class_1(1)
+class_1_0 = Class_1(0)
+class_1_1 = Class_1(1)
 class_1_0.class_variable.extend([2])
 print("class_1_0.class_variable = " + str(class_1_0.class_variable) + "; class_1_0.instance_variable = " + str(class_1_0.instance_variable))
 print("class_1_1.class_variable = " + str(class_1_1.class_variable) + "; class_1_1.instance_variable = " + str(class_1_1.instance_variable))
 
 print("<<dynamic function>>")
-class class_2:
+class Class_2:
     def function_0(self):
         pass
-class_2_0 = class_2()
+class_2_0 = Class_2()
 class_2_0.function_0 = sorted
 class_2_0.function_1 = sum
 print(class_2_0.function_0([1,5,3,2,4]))
 print(class_2_0.function_1([1,5,3,2,4]))
 
+print("<<inheritance>>")
+class Parent_0:
+    parentName = "parent_0"
+    def getParentName(self):
+        return self.parentName
 
+class Child_0(Parent_0):
+    childName = "child_0"
+    def getChildName(self):
+        return Parent_0.getParentName(self) + "_" + self.childName
 
+child_0_0 = Child_0()
+print("parent name = " + child_0_0.getParentName() + "; child name = " + child_0_0.getChildName())
+
+print("<<inner class>>")
+class Outer_0:
+    name = "Outer_0"
+    def getName(self):
+        return self.name
+
+    class Inner_0():
+        name = "Inner_0"
+        def getName(self):
+            return self.name
+
+print("Inner_0 name = " + Outer_0().Inner_0().getName())
--- python-example/function/build_in.py
+++ python-example/function/build_in.py
@@ -10,4 +10,20 @@ print(dir("string"))
 print("<<type>>")
 print(type("string"))
 
+print("<<isinstance>>")
+class Obj_0:
+    def __init__(self):
+        pass
 
+print(isinstance(Obj_0(), Obj_0))
+
+print("<<issubclass>>")
+class Obj_1_0:
+    def __init__(self):
+        pass
+
+class Obj_1_1(Obj_1_0):
+    def __init__(self):
+        pass
+
+print(issubclass(Obj_1_1, Obj_1_0))
