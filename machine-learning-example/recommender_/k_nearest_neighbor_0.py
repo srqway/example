9126a4f56d7dfdae725173af1ece067b7914922c
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-05-11 00:07:17 +0800

    backup.

--- machine-learning-example/recommender_/k_nearest_neighbor_0.py
+++ machine-learning-example/recommender_/k_nearest_neighbor_0.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-'''
-If the data is subject to grade-inflation (different users may be using different scales) use Pearson.
-'''
+
 from __future__ import division
 from __future__ import print_function
 from math import sqrt
 
 class Recommender(object):
+    def __init__(self, data, k=1, n=5, metric='pearson'):
+        if type(data).__name__ == 'dict':
+            self.data = data
+        self.k = k
+        self.n = n
+        if metric == 'pearson':
+            self.fn = self.pearson
+#         self.username2id = {}
+#         self.userid2name = {}
+#         self.productid2name = {}
+#         self.metric = metric 
+        
     def pearson(self, rating1, rating2):
         sum_xy = 0
         sum_x = 0
@@ -32,25 +42,37 @@ class Recommender(object):
             return 0
         return (sum_xy - (sum_x * sum_y) / n) / denominator
     
-    def computeNearestNeighbor(self, username, users):
+    def computeNearestNeighbor(self, user):
         distances = []
-        for user in users:
-            if user != username:
-                distance = self.pearson(users[user], users[username])
-                distances.append((distance, user))
-        distances.sort()
+        for instance in self.data:
+            if instance != user:
+                distance = self.fn(self.data[user], self.data[instance])
+                distances.append((instance, distance))
+        distances.sort(key=lambda artistTuple: artistTuple[1], reverse=True)
         return distances
 
-    def recommend(self, username, users):
-        nearest = self.computeNearestNeighbor(username, users)[0][1]
-        recommendations = []
-        neighborRatings = users[nearest]
-        userRatings = users[username]
-        for artist in neighborRatings:
-            if not artist in userRatings:
-                recommendations.append((artist, neighborRatings[artist]))
-        return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse=True)
-        
+    def recommend(self, user):
+        recommendations = {}
+        nearest = self.computeNearestNeighbor(user)
+        userRatings = self.data[user]
+        totalDistance = 0.0
+        for i in range(self.k):
+            totalDistance += nearest[i][1]
+        for i in range(self.k):
+            weight = nearest[i][1] / totalDistance
+            name = nearest[i][0]
+            neighborRatings = self.data[name]
+            for artist in neighborRatings:
+                if not artist in userRatings:
+                    if artist not in recommendations:
+                        recommendations[artist] = (neighborRatings[artist] * weight)
+                    else:
+                        recommendations[artist] = (recommendations[artist] + neighborRatings[artist] * weight)
+
+        rec_list = list(recommendations.items())
+        rec_list.sort(key=lambda artistTuple: artistTuple[1], reverse = True)
+        return rec_list[:self.n]
+
 if __name__ == '__main__':
     users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0,
                       "Norah Jones": 4.5, "Phoenix": 5.0,
@@ -88,6 +110,7 @@ if __name__ == '__main__':
                       "The Strokes": 3.0}
         }
     
-    recommender = Recommender()
-    print(recommender.recommend("Hailey", users))
+
+    recommender = Recommender(users)
+    print(recommender.recommend("Hailey"))
     
