2470922646c050a0ff3421fb2ad41d5789e25544
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-06-19 09:02:53 +0800

    backup.

--- python-example/basic_/for_.py
+++ python-example/basic_/for_.py
@@ -3,6 +3,7 @@
 
 from __future__ import division
 from __future__ import print_function
+import re
 
 print("<<for i, n in enumerate([1, 3, 5]):>>")
 for (i, n) in enumerate([1, 3, 5]):
@@ -13,3 +14,6 @@ names_0 = ["John", "Marry", "Tom"]
 sexes_0 = ["Male", "Female", "Male"]
 for (name, sex) in zip(names_0, sexes_0):
     print(name, sex)
+
+print("<<[x for x in xrange(n)]>>")   
+print([x for x in xrange(n)])
