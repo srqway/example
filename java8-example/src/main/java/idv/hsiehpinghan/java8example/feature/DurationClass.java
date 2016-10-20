0478b0726d2b49771451a251d57f7affbbc12c82
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-10-20 10:21:26 +0800

    backup.

--- java8-example/src/main/java/idv/hsiehpinghan/java8example/feature/DurationClass.java
+++ java8-example/src/main/java/idv/hsiehpinghan/java8example/feature/DurationClass.java
@@ -2,11 +2,16 @@ package idv.hsiehpinghan.java8example.feature;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
 
 public class DurationClass {
 
-	public Duration getDuration(LocalDateTime LocalDateTime_0,
-			LocalDateTime LocalDateTime_1) {
+	public Duration getDuration(LocalDateTime LocalDateTime_0, LocalDateTime LocalDateTime_1) {
 		return Duration.between(LocalDateTime_0, LocalDateTime_1);
 	}
+
+	public Duration getDuration(long amount, TemporalUnit unit) {
+		return Duration.of(amount, unit);
+	}
+
 }
--- java8-example/src/test/java/idv/hsiehpinghan/java8example/feature/DurationClassTest.java
+++ java8-example/src/test/java/idv/hsiehpinghan/java8example/feature/DurationClassTest.java
@@ -1,9 +1,9 @@
 package idv.hsiehpinghan.java8example.feature;
 
-import idv.hsiehpinghan.java8example.feature.DurationClass;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,14 +12,20 @@ public class DurationClassTest {
 	private DurationClass durationClass = new DurationClass();
 
 	@Test
-	public void getDuration() {
-		LocalDateTime LocalDateTime_0 = LocalDateTime
-				.parse("2015-01-01T18:30:45.000");
-		LocalDateTime LocalDateTime_1 = LocalDateTime
-				.parse("2015-01-01T18:30:46.235");
-		Duration duration = durationClass.getDuration(LocalDateTime_0,
-				LocalDateTime_1);
+	public void getDuration_0() {
+		LocalDateTime LocalDateTime_0 = LocalDateTime.parse("2015-01-01T18:30:45.000");
+		LocalDateTime LocalDateTime_1 = LocalDateTime.parse("2015-01-01T18:30:46.235");
+		Duration duration = durationClass.getDuration(LocalDateTime_0, LocalDateTime_1);
 		long actual = duration.getSeconds();
 		Assert.assertEquals(actual, 1);
+	}
+
+	@Test
+	public void getDuration_1() {
+		long amount = 2592000L;
+		TemporalUnit unit = ChronoUnit.SECONDS;
+		Duration duration = durationClass.getDuration(amount, unit);
+		long actual = duration.toDays();
+		Assert.assertEquals(actual, 30);
 	}
 }
