a8aac803750df0a10febf9cbf6ab60a38d2fe8d2
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-12-13 12:49:37 +0800

    backup.

--- scala-example/src/main/scala/idv/hsiehpinghan/scalaexample/example/File_.scala
+++ scala-example/src/main/scala/idv/hsiehpinghan/scalaexample/example/File_.scala
@@ -2,8 +2,14 @@ package idv.hsiehpinghan.scalaexample.example
 
 import scala.collection.immutable.List
 import scala.io.Source
+import scala.reflect.io.File
 
 class File_ {
+  def delete(path: String) {
+    val file = File(path)
+    file.delete()
+  }
+    
   def file_0() = {
     var f = Source.fromFile("/etc/passwd");
     for(l <- f.getLines()) {
