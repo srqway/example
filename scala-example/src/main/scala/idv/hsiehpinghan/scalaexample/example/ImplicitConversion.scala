9a099021156267a6859bd1bec0efa9d95cfab9a9
hsiehpinghan <hsiehpinghan@thank>
2017-01-04 23:23:17 +0800

    backup.

--- scala-example/src/main/scala/idv/hsiehpinghan/scalaexample/example/ImplicitConversion.scala
+++ scala-example/src/main/scala/idv/hsiehpinghan/scalaexample/example/ImplicitConversion.scala
@@ -1,8 +1,10 @@
 package idv.hsiehpinghan.scalaexample.example
 
+import idv.hsiehpinghan.scalaexample.example.ConvertingTheReceiver.convertingTheReceiver_0
+
 class FunctionToClassClass() {
   var function: Int => String = null
-  
+
   def method(int: Int) {
     println("FunctionToClassClass method %s".format(function(int)))
   }
@@ -13,7 +15,7 @@ object FunctionToClassClass {
     clazz.function = function
     clazz
   }
-  
+
   implicit def functionToClass_1(function: Int => String) = {
     val clazz = new FunctionToClassClass()
     clazz.function = function
@@ -21,19 +23,49 @@ object FunctionToClassClass {
   }
 }
 
+class ConvertingTheReceiver(var num: Int = 0) {
+  def +(n: Int): ConvertingTheReceiver = {
+    new ConvertingTheReceiver(num + n)
+  }
+  def +(that: ConvertingTheReceiver): ConvertingTheReceiver = {
+    new ConvertingTheReceiver(this.num + that.num)
+  }
+}
+object ConvertingTheReceiver {
+  implicit def convertingTheReceiver_0(x: Int) = {
+    new ConvertingTheReceiver(x)
+  }
+}
+
+class ImplicitParameters(val name: String = null) {
+  def implicitParameters(name: String)(implicit parameter: ImplicitParameters) {
+    println("%s / %s".format(name, parameter.name))
+  }
+}
+object ImplicitParameters {
+  implicit val parameter = new ImplicitParameters("parameter")
+}
+
 object ImplicitConversion_Main extends App {
-//  import idv.hsiehpinghan.scalaexample.example.FunctionToClassClass.functionToClass_0
-//  def functionToClassFunction_0 = (int: Int) => {
-//    int.toString()
-//  }
-//  def functionToClassTest(clazz: FunctionToClassClass) = {
-//    clazz.method(3)
-//  }
-//  functionToClassTest(functionToClassFunction_0)
-  
-//  import idv.hsiehpinghan.scalaexample.example.FunctionToClassClass.functionToClass_1
-//  def functionToClassFunction_1 = (int: Int) => {
-//    int.toString()
-//  }
-//  functionToClassFunction_1.method(3)
+  //  import idv.hsiehpinghan.scalaexample.example.FunctionToClassClass.functionToClass_0
+  //  def functionToClassFunction_0 = (int: Int) => {
+  //    int.toString()
+  //  }
+  //  def functionToClassTest(clazz: FunctionToClassClass) = {
+  //    clazz.method(3)
+  //  }
+  //  functionToClassTest(functionToClassFunction_0)
+
+  //  import idv.hsiehpinghan.scalaexample.example.FunctionToClassClass.functionToClass_1
+  //  def functionToClassFunction_1 = (int: Int) => {
+  //    int.toString()
+  //  }
+  //  functionToClassFunction_1.method(3)
+
+//  val convertingTheReceiver = new ConvertingTheReceiver
+//  val c = 3 + convertingTheReceiver
+//  println(c.num)
+
+  val implicitParameters = new ImplicitParameters
+  implicitParameters.implicitParameters("implicitParameters")
 }
